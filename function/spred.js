79e9bfaaf1a920dd9d8dee0e648cc639eaf048b2
Kamal Barman <kamalbarman1528@gmail.com>
2024-02-28 08:33:27 +0530

    modified the spred,js file

--- function/spred.js
+++ function/spred.js
@@ -1,3 +1,10 @@
+// spread use to:
+//  Expanding Arrays
+//  Extracting Elements from Arrays
+//  Working with Objects
+//  Function Arguments
+
+
 let arr =[100,44,56,43,49];
 let newArr = {...arr};
 console.log(newArr);
