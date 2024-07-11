d14576b9a280ae9fcd1cd03a9fb07916867110ce
Kamal Barman <kamalbarman1528@gmail.com>
2024-07-12 01:41:06 +0530

    generate user define random number(api/index.html and jokes.js)

--- Api/jokes.js
+++ Api/jokes.js
@@ -10,36 +10,55 @@
 //     }
 // }
 
-let url = "http://universities.hipolabs.com/search?name=";
-let btn = document.querySelector("button");
+// let url = "http://universities.hipolabs.com/search?name=";
+// let btn = document.querySelector("button");
+// console.log(btn);
 
 
-btn.addEventListener("click",async ()=>{
-   let country = document.querySelector("input").value;
-   console.log(country);
-   let colleges = await getColleges(country);
-   console.log(colleges);
+// btn.addEventListener("click",async ()=>{
+   
+//    let country = document.querySelector("input").value;
+//    console.log(country);
+//    let colleges = await getColleges(country);
+//    console.log(colleges);
 
-   show(colleges);
-});
+//    show(colleges);
+// });
 
-function show(colleges){
-    let list = document.querySelector("#list");
-    for (col of colleges) {
-        console.log(col.name);
-        let li = document.createElement("li");
-        li.innerText = col.name;
-        list.appendChild(li);
+// function show(colleges){
+//     let list = document.querySelector("#list");
+//     for (col of colleges) {
+//         console.log(col.name);
+//         let li = document.createElement("li");
+//         li.innerText = col.name;
+//         list.appendChild(li);
+//     }
+// }
+
+// async function getColleges(country){
+//     try{
+//         let res = await axios.get(url+ country);
+//         // console.log(res);
+//         return res.data;
+//     }catch(err){
+//         console.log("error : ",err);
+//         return [];
+//     }
+// }
+
+function generateRandomNumber() {
+    const minNumber = parseInt(document.getElementById("minNumber").value);
+    const maxNumber = parseInt(document.getElementById("maxNumber").value);
+  
+    // Input validation (optional)
+    if (minNumber >= maxNumber) {
+      alert("type number under your range!");
+      return;
     }
-}
-
-async function getColleges(country){
-    try{
-        let res = await axios.get(url+ country);
-        // console.log(res);
-        return res.data;
-    }catch(err){
-        console.log("error : ",err);
-        return [];
-    }
-}
+  
+    // Generate random number within the range
+    const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  
+    // Display the generated number
+    document.getElementById("randomNumberDisplay").textContent = `Random number: ${randomNumber}`;
+  }
---
+++ noe-js/app.js
@@ -0,0 +1,7 @@
+// let url = "http://universities.hipolabs.com/search?name=";
+
+// let btn = document.querySelector("button");
+
+const someValue = require("./math");
+
+console.log(someValue);
